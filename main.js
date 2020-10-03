249e15984b81c9c9fc2a3b1ab285da6abf122082
ifrahv <ifrah.vermeer@gmail.com>
2020-10-03 18:18:32 +0100

    updated

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const APIURL =
     "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
@@ -32,7 +33,7 @@ function showMovies(movies) {
 
         const movieEl = document.createElement("div");
         movieEl.classList.add("movie");
-        
+
 
         movieEl.innerHTML = `
             <img
